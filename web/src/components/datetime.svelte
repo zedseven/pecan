973e53770449e603904fe1759fa760c9cf387a40
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2022-11-04 02:47:36 -0400

    Improved the datetime display so that it only shows the time instead of the date if it's currently the same day as the datetime value.

--- web/src/components/datetime.svelte
+++ web/src/components/datetime.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-	// Constants
-	const millisPerDay = 24 * 60 * 60 * 1000;
-
 	// Component Data
 	export let datetimeUtc;
 
 	// Renders the UTC ISO 8601 datetime as a locale-specific local time
 	const renderDatetime = function (datetimeUtcStr) {
 		const parsedDate = new Date(datetimeUtcStr); // Technically not a good idea, but it should be fine
-		const elapsedMillis = new Date() - parsedDate;
+		const currentDate = new Date();
 
-		// Display the time if it's been less than 24h, otherwise display the date
-		if (elapsedMillis < millisPerDay) return parsedDate.toLocaleTimeString();
+		// Display the time if it's currently the same day, otherwise display the date
+		if (
+			parsedDate.getFullYear() == currentDate.getFullYear() &&
+			parsedDate.getMonth() == currentDate.getMonth() &&
+			parsedDate.getDate() == currentDate.getDate()
+		)
+			return parsedDate.toLocaleTimeString();
 		else return parsedDate.toLocaleDateString();
 	};
 </script>
